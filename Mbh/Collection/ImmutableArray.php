61e3981cd667809ce02e3559ac16412eb01152ed
ulises-jeremias <ulisescf.24@gmail.com>
2017-11-29 17:41:00 -0300

    Walk and filter methods added

--- Mbh/Collection/ImmutableArray.php
+++ Mbh/Collection/ImmutableArray.php
@@ -58,18 +58,58 @@ class ImmutableArray implements Iterator, ArrayAccess, Countable, JsonSerializab
     /**
      * Map elements to a new ImmutableArray via a callback
      *
-     * @param callable $cb Function to map new data
+     * @param callable $callback Function to map new data
      * @return ImmutableArray
      */
-    public function map(callable $cb): self
+    public function map(callable $callback): self
     {
         $count = count($this);
         $sfa = new SplFixedArray($count);
 
         for ($i = 0; $i < $count; $i++) {
-            $sfa[$i] = $cb($this->sfa[$i], $i, $this);
+            $sfa[$i] = $callback($this->sfa[$i], $i, $this);
         }
 
+        return new static($sfa);
+    }
+
+    /**
+     * forEach, or "walk" the data
+     * Exists primarily to provide a consistent interface, though it's seldom
+     * any better than a simple php foreach. Mainly useful for chaining.
+     * Named walk for historic reasons - forEach is reserved in PHP
+     *
+     * @param callable $callback Function to call on each element
+     * @return ImmutableArray
+     */
+    public function walk(callable $callback): self
+    {
+        foreach ($this as $i => $elem) {
+            $callback($elem, $i, $this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Filter out elements
+     *
+     * @param callable $callback Function to filter out on false
+     * @return ImmutableArray
+     */
+    public function filter(callable $callback): self
+    {
+        $count = count($this->sfa);
+        $sfa = new SplFixedArray($count);
+        $newCount = 0;
+
+        foreach ($this->sfa as $el) {
+            if ($callback($el)) {
+                $sfa[$newCount++] = $el;
+            }
+        }
+
+        $sfa->setSize($newCount);
         return new static($sfa);
     }
 }
