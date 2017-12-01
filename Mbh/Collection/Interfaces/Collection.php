d4c886798fea99acd33354346b482269ecb4c08b
ulises-jeremias <ulisescf.24@gmail.com>
2017-12-01 13:33:50 -0300

    Update

--- Mbh/Collection/Interfaces/Collection.php
+++ Mbh/Collection/Interfaces/Collection.php
@@ -14,13 +14,13 @@ use JsonSerializable;
 use Traversable;
 
 /**
-  * Collection is the base interface which covers functionality common to all the
-  * data structures in this library. It guarantees that all structures are
-  * traversable, countable, and can be converted to json using json_encode().
-  *
-  * @package structures
-  * @author Ulises Jeremias Cornejo Fandos <ulisescf.24@gmail.com>
-  */
+ * Collection is the base interface which covers functionality common to all the
+ * data structures in this library. It guarantees that all structures are
+ * traversable, countable, and can be converted to json using json_encode().
+ *
+ * @package structures
+ * @author Ulises Jeremias Cornejo Fandos <ulisescf.24@gmail.com>
+ */
 
 interface Collection extends Iterator, Countable, JsonSerializable
 {
