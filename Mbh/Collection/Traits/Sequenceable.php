d95d50f0196fa47e5a0c1fdffddebeab737c3fee
ulises-jeremias <ulisescf.24@gmail.com>
2017-12-03 20:22:54 -0300

    Sequenceable trait added

---
+++ Mbh/Collection/Traits/Sequenceable.php
@@ -0,0 +1,217 @@
+<?php namespace Mbh\Collection\Traits;
+
+use Mbh\Collection\Interfaces\Collection as CollectionInterface;
+use Mbh\Collection\Interfaces\Sequenceable as SequenceableInterface;
+use Mbh\Collection\CallbackHeap;
+use Mbh\Iterator\SliceIterator;
+use Mbh\Iterator\ConcatIterator;
+use SplFixedArray;
+use SplHeap;
+use SplStack;
+use LimitIterator;
+use Iterator;
+use ArrayAccess;
+use Countable;
+use CallbackFilterIterator;
+use JsonSerializable;
+use RuntimeException;
+use Traversable;
+use ReflectionClass;
+
+/**
+ * MBHFramework
+ *
+ * @link      https://github.com/MBHFramework/mbh-framework
+ * @copyright Copyright (c) 2017 Ulises Jeremias Cornejo Fandos
+ * @license   https://github.com/MBHFramework/mbh-framework/blob/master/LICENSE (MIT License)
+ */
+
+trait Sequenceable
+{
+    use Collection;
+    use Sort {
+        Sort::heapSort as heapSortWithCallback;
+    }
+
+    /**
+     * Map elements to a new Sequenceable via a callback
+     *
+     * @param callable $callback Function to map new data
+     * @return Sequenceable
+     */
+    public function map(callable $callback): self
+    {
+        $count = count($this);
+        $sfa = new SplFixedArray($count);
+
+        for ($i = 0; $i < $count; $i++) {
+            $sfa[$i] = $callback($this->sfa[$i], $i, $this);
+        }
+
+        return new static($sfa);
+    }
+
+    /**
+     * forEach, or "walk" the data
+     * Exists primarily to provide a consistent interface, though it's seldom
+     * any better than a simple php foreach. Mainly useful for chaining.
+     * Named walk for historic reasons - forEach is reserved in PHP
+     *
+     * @param callable $callback Function to call on each element
+     * @return Sequenceable
+     */
+    public function walk(callable $callback): self
+    {
+        foreach ($this as $i => $elem) {
+            $callback($elem, $i, $this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Filter out elements
+     *
+     * @param callable $callback Function to filter out on false
+     * @return Sequenceable
+     */
+    public function filter(callable $callback): self
+    {
+        $count = count($this);
+        $sfa = new SplFixedArray($count);
+        $newCount = 0;
+
+        foreach ($this as $elem) {
+            if ($callback($elem)) {
+                $sfa[$newCount++] = $elem;
+            }
+        }
+
+        $sfa->setSize($newCount);
+        return new static($sfa);
+    }
+
+    /**
+     * Reduce to a single value
+     *
+     * @param callable $callback Callback(
+     *     mixed $previous, mixed $current[, mixed $index, mixed $immArray]
+     * ):mixed Callback to run reducing function
+     * @param mixed $accumulator Initial value for first argument
+     */
+    public function reduce(callable $callback, $accumulator = null)
+    {
+        foreach ($this as $i => $elem) {
+            $accumulator = $callback($accumulator, $elem, $i, $this);
+        }
+
+        return $accumulator;
+    }
+
+    /**
+     * Join a set of strings together.
+     *
+     * @param string $token Main token to put between elements
+     * @param string $secondToken If set, $token on left $secondToken on right
+     * @return string
+     */
+    public function join(string $token = ',', string $secondToken = null): string
+    {
+        $str = "";
+        if ($secondToken) {
+            foreach ($this as $i => $elem) {
+                $str .= $token . (string) $elem . $secondToken;
+            }
+        } else {
+            $this->rewind();
+            while ($this->valid()) {
+                $str .= (string) $this->current();
+                $this->next();
+                if ($this->valid()) {
+                    $str .= $token;
+                }
+            }
+        }
+
+        return $str;
+    }
+
+    /**
+     * Take a slice of the array
+     *
+     * @param int $begin Start index of slice
+     * @param int $end End index of slice
+     * @return Sequenceable
+     */
+    public function slice(int $begin = 0, int $end = null): self
+    {
+        $it = new SliceIterator($this, $begin, $end);
+        return new static($it);
+    }
+
+    /**
+     * Concat to the end of this array
+     *
+     * @param Traversable,...
+     * @return Sequenceable
+     */
+    public function concat(): self
+    {
+        $args = func_get_args();
+        array_unshift($args, $this);
+
+        // Concat this iterator, and variadic args
+        $class = new ReflectionClass('Mbh\Iterator\ConcatIterator');
+        $concatIt = $class->newInstanceArgs($args);
+
+        // Create as new immutable's iterator
+        return new static($concatIt);
+    }
+
+    /**
+     * Find a single element
+     *
+     * @param callable $callback The test to run on each element
+     * @return mixed The element we found
+     */
+    public function find(callable $callback)
+    {
+        foreach ($this as $i => $elem) {
+            if ($callback($elem, $i, $this)) {
+                return $elem;
+            }
+        }
+    }
+
+    /**
+     * Return a new sorted Sequenceable
+     *
+     * @param callable $callback The sort callback
+     * @return Sequenceable
+     */
+    public function sort(callable $callback = null)
+    {
+        if ($callback) {
+            return $this->mergeSort($callback);
+        }
+
+        return $this->arraySort();
+    }
+
+    /**
+     * Sort a new Sequenceable by filtering through a heap.
+     * Tends to run much faster than array or merge sorts, since you're only
+     * sorting the pointers, and the sort function is running in a highly
+     * optimized space.
+     *
+     * @param SplHeap $heap The heap to run for sorting
+     * @return Sequenceable
+     */
+    public function heapSort(SplHeap $heap): self
+    {
+        foreach ($this as $item) {
+            $heap->insert($item);
+        }
+        return static::fromItems($heap);
+    }
+}
