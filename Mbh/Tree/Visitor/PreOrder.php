54d3257f8f00039f85079d2440dd0787e2a0290f
ulises-jeremias <ulisescf.24@gmail.com>
2017-12-10 12:31:45 -0300

    Pre Order visitor fix

--- Mbh/Tree/Visitor/PreOrder.php
+++ Mbh/Tree/Visitor/PreOrder.php
@@ -3,12 +3,12 @@
 use Mbh\Tree\Interfaces\Node as NodeInterface;
 use Mbh\Tree\Visitor;
 
-class PostOrder extends Visitor
+class PreOrder extends Visitor
 {
     public function visit(NodeInterface $node)
     {
         $nodes = [$node];
-        
+
         foreach ($node->getChildren() as $child) {
             $nodes = array_merge(
                 $nodes,
