aa2bc9d766147bb25970837be81f77ed76d52130
ulises-jeremias <ulisescf.24@gmail.com>
2017-12-11 23:18:36 -0300

    Deque and Squared Capacity added

---
+++ Mbh/Collection/Deque.php
@@ -0,0 +1,32 @@
+<?php namespace Mbh\Collection;
+
+/**
+ * MBHFramework
+ *
+ * @link      https://github.com/MBHFramework/mbh-framework
+ * @copyright Copyright (c) 2017 Ulises Jeremias Cornejo Fandos
+ * @license   https://github.com/MBHFramework/mbh-framework/blob/master/LICENSE (MIT License)
+ */
+
+use Mbh\Collection\Interfaces\Sequenceable as SequenceableInterface;
+
+/**
+ * A Deque (pronounced "deck") is a sequence of values in a contiguous buffer
+ * that grows and shrinks automatically. The name is a common abbreviation of
+ * "double-ended queue".
+ *
+ * While a Deque is very similar to a Vector, it offers constant time operations
+ * at both ends of the buffer, ie. shift, unshift, push and pop are all O(1).
+ *
+ * @package structures
+ * @author Ulises Jeremias Cornejo Fandos <ulisescf.24@gmail.com>
+ */
+class Deque implements SequenceableInterface
+{
+    use Traits\Collection;
+    use Traits\Sequenceable;
+    use Traits\Functional;
+    use Traits\SquaredCapacity;
+
+    const MIN_CAPACITY = 8.0;
+}
---
+++ Mbh/Collection/Traits/SquaredCapacity.php
@@ -0,0 +1,57 @@
+<?php namespace Mbh\Collection\Traits;
+
+/**
+ * MBHFramework
+ *
+ * @link      https://github.com/MBHFramework/mbh-framework
+ * @copyright Copyright (c) 2017 Ulises Jeremias Cornejo Fandos
+ * @license   https://github.com/MBHFramework/mbh-framework/blob/master/LICENSE (MIT License)
+ */
+
+ /**
+  * Common to structures that require a capacity which is a power of two.
+  */
+trait SquaredCapacity
+{
+    use Capacity;
+
+    /**
+     * Rounds an integer to the next power of two if not already a power of two.
+     *
+     * @param int $capacity
+     *
+     * @return int
+     */
+    private function square(int $capacity): int
+    {
+        return pow(2, ceil(log($capacity, 2)));
+    }
+
+    /**
+     * Ensures that enough memory is allocated for a specified capacity. This
+     * potentially reduces the number of reallocations as the size increases.
+     *
+     * @param int $capacity The number of values for which capacity should be
+     *                      allocated. Capacity will stay the same if this value
+     *                      is less than or equal to the current capacity.
+     */
+    public function allocate(int $capacity)
+    {
+        $this->capacity = max($this->square($capacity), $this->capacity);
+    }
+
+    /**
+     * Called when capacity should be increased to accommodate new values.
+     */
+    protected function increaseCapacity()
+    {
+        $this->capacity = $this->square(max($this->count() + 1, $this->capacity * $this->getGrowthFactor()));
+    }
+
+    /**
+     * Gets the size of the array.
+     *
+     * @return int
+     */
+    abstract protected function count(): int;
+}
