b19c8908fbd8c024ab182b7c3cec41aa61aa643c
ulises-jeremias <ulisescf.24@gmail.com>
2017-12-22 16:28:10 -0300

    Update

--- Mbh/Collection/ImmutableArray.php
+++ Mbh/Collection/ImmutableArray.php
@@ -27,21 +27,21 @@ class ImmutableArray extends FixedArray
 {
     public function sort(callable $callback = null): SequenceableInterface
     {
-        throw ImmutableException::cannotModify(__CLASS__, 'sort');
+        throw ImmutableException::cannotModify(__CLASS__, __METHOD__);
     }
 
     public function offsetSet($offset, $value)
     {
-        throw ImmutableException::cannotModify(__CLASS__, 'offsetSet');
+        throw ImmutableException::cannotModify(__CLASS__, __METHOD__);
     }
 
     public function offsetUnset($offset)
     {
-        throw ImmutableException::cannotModify(__CLASS__, 'offsetUnset');
+        throw ImmutableException::cannotModify(__CLASS__, __METHOD__);
     }
 
     public function clear()
     {
-        throw ImmutableException::cannotModify(__CLASS__, 'clear');
+        throw ImmutableException::cannotModify(__CLASS__, __METHOD__);
     }
 }
