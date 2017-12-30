a811c57ae03738a2ba5ddd5faf3ecd37cedc1c9b
ulises-jeremias <ulisescf.24@gmail.com>
2017-12-30 01:08:11 -0300

    Added some Functional methods

--- Mbh/Collection/Interfaces/Functional.php
+++ Mbh/Collection/Interfaces/Functional.php
@@ -20,6 +20,14 @@ use SplHeap;
 interface Functional
 {
     /**
+     *
+     *
+     * @param callable $callback Function to map new data
+     * @return Collection
+     */
+    public function any(callable $callback);
+
+    /**
      * Concat to the end of this array
      *
      * @param Traversable,...
@@ -44,7 +52,7 @@ interface Functional
     public function filter(callable $callback);
 
     /**
-     * Sort a new Collection by filtering through a heap.
+     * Sort the Collection by filtering through a heap.
      * Tends to run much faster than array or merge sorts, since you're only
      * sorting the pointers, and the sort function is running in a highly
      * optimized space.
@@ -53,6 +61,17 @@ interface Functional
      * @return Collection
      */
     public function heapSort(SplHeap $heap);
+
+    /**
+     * Sort a new Collection by filtering through a heap.
+     * Tends to run much faster than array or merge sorts, since you're only
+     * sorting the pointers, and the sort function is running in a highly
+     * optimized space.
+     *
+     * @param SplHeap $heap The heap to run for sorting
+     * @return Collection
+     */
+    public function heapSorted(SplHeap $heap);
 
     /**
      * Join a set of strings together.
@@ -91,12 +110,30 @@ interface Functional
     public function slice(int $begin = 0, int $length = null);
 
     /**
-     * Return a new sorted Collection
+     * Remove a portion of the array and replace it with something else
+     *
+     * @param int $begin Start index of slice
+     * @param int $end End index of slice
+     * @param mixed $replacement
+     * @return Collection
+     */
+    public function splice(int $begin = 0, int $length = null, $replacement = []);
+
+    /**
+     * Sort the Collection
      *
      * @param callable $callback The sort callback
      * @return Collection
      */
     public function sort(callable $callback = null);
+
+    /**
+     * Return a new sorted Collection
+     *
+     * @param callable $callback The sort callback
+     * @return Collection
+     */
+    public function sorted(callable $callback = null);
 
     /**
      * forEach, or "walk" the data
