ccba1d11155bbf1662c56dc78a5d83c1dfe57e54
ulises-jeremias <ulisescf.24@gmail.com>
2018-01-01 21:21:39 -0300

    Fixed

--- Mbh/Collection/Map.php
+++ Mbh/Collection/Map.php
@@ -33,6 +33,7 @@ class Map implements AllocatedInterface, ArrayAccess, CollectionInterface, Itera
 {
     use Traits\Collection;
     use Traits\Functional;
+    use Traits\Builder;
     use SquaredCapacity;
     use EmptyGuard;
 
@@ -195,7 +196,7 @@ class Map implements AllocatedInterface, ArrayAccess, CollectionInterface, Itera
      */
     public function keys(): Set
     {
-        return new Set($this->pairs->map(function($pair) {
+        return new Set($this->pairs->map(function ($pair) {
             return $pair->key;
         }));
     }
@@ -270,7 +271,7 @@ class Map implements AllocatedInterface, ArrayAccess, CollectionInterface, Itera
      */
     public function pairs(): SequenceableInterface
     {
-        return $this->pairs->map(function($pair) {
+        return $this->pairs->map(function ($pair) {
             return $pair->copy();
         });
     }
@@ -372,7 +373,7 @@ class Map implements AllocatedInterface, ArrayAccess, CollectionInterface, Itera
      */
     public function values(): SequenceableInterface
     {
-        return $this->pairs->map(function($pair) {
+        return $this->pairs->map(function ($pair) {
             return $pair->value;
         });
     }
