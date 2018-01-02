72155301527af8995ccbdccd57276c60f2f551c5
ulises-jeremias <ulisescf.24@gmail.com>
2018-01-01 23:27:54 -0300

    Updated DoublyLinkedList tests

--- tests/Collection/DoublyLinkedListTest.php
+++ tests/Collection/DoublyLinkedListTest.php
@@ -61,4 +61,123 @@ class DoublyLinkedListTest extends TestCase
 
         $this->assertEquals($other, $list->toArray());
     }
+
+    public function testInsert()
+    {
+        $base = [1, 2, 3, 4, 7];
+        $other = [1, 2, 3, 4, 5, 6, 7];
+
+        $numberSet = DoublyLinkedList::fromArray($base);
+
+        $numberSet->insert(4, 5, 6);
+
+        $this->assertEquals($other, $numberSet->toArray());
+    }
+
+    public function testMap()
+    {
+        $base = [1, 2, 3, 4];
+        $doubled = [2, 4, 6, 8];
+
+        $numberSet = DoublyLinkedList::fromArray($base);
+        $mapped = $numberSet->map(function ($num) {
+            return $num * 2;
+        });
+
+        foreach ($mapped as $i => $v) {
+            $this->assertEquals($v, $doubled[$i]);
+        }
+    }
+
+    public function testContains()
+    {
+        $base = [1, 2, 3, 4, 5, 6, 7];
+        $other = [1, 2, 3, 4, 7];
+
+        $numberSet = DoublyLinkedList::fromArray($base);
+
+        $contains = $numberSet->contains($other);
+
+        $this->assertEquals(true, $contains);
+    }
+
+    public function testFilter()
+    {
+        $oddArr = [1, 3, 5, 7, 9];
+        $fixedArr = DoublyLinkedList::fromArray([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+        $odds = $fixedArr->filter(function ($num) {
+            return $num % 2;
+        });
+
+        foreach ($odds as $i => $v) {
+            $this->assertEquals($v, $oddArr[$i]);
+        }
+    }
+
+    public function testReduce()
+    {
+        $arIt = new ArrayIterator([1, 2, 3, 4, 5]);
+        $numberSet = DoublyLinkedList::fromItems($arIt);
+
+        // Reduce with sum
+        $sum = $numberSet->reduce(function ($last, $cur) {
+            return $last + $cur;
+        }, 0);
+
+        $this->assertEquals(15, $sum);
+
+        // Reduce with string concat
+        $concatted = $numberSet->reduce(function ($last, $cur, $i) {
+            return $last . '{"'. $i . '":"' . $cur . '"},';
+        }, '');
+
+        $this->assertEquals('{"0":"1"},{"1":"2"},{"2":"3"},{"3":"4"},{"4":"5"},', $concatted);
+    }
+
+    public function testJoin()
+    {
+        $imarr = DoublyLinkedList::fromArray(['foo', 'bar', 'baz']);
+
+        $this->assertEquals('foo,bar,baz', $imarr->join(), 'Default join failed.');
+        $this->assertEquals('fooXXXbarXXXbaz', $imarr->join('XXX'), 'Token join failed.');
+        $this->assertEquals('<li>foo</li><li>bar</li><li>baz</li>', $imarr->join('<li>', '</li>'), 'Two token join failed.');
+    }
+
+    public function testSlice()
+    {
+        $fixedArr = DoublyLinkedList::fromArray([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+        $firstThree = $fixedArr->slice(0, 3);
+
+        $this->assertCount(3, $firstThree);
+        $this->assertSame([1, 2, 3], $firstThree->toArray());
+    }
+
+    public function testConcat()
+    {
+        $setA = DoublyLinkedList::fromArray([1, 2, 3]);
+        $setB = DoublyLinkedList::fromItems(new ArrayIterator([4, 5, 6]));
+
+        $concatted = $setA->concat($setB);
+        $this->assertSame([1, 2, 3, 4, 5, 6], $concatted->toArray());
+    }
+
+    public function testSorted()
+    {
+        $unsorted = DoublyLinkedList::fromArray(['f', 'c', 'a', 'b', 'e', 'd']);
+        $sorted = $unsorted->sorted(function ($a, $b) {
+            return strcmp($a, $b);
+        });
+
+        $this->assertSame($sorted->toArray(), ['a', 'b', 'c', 'd', 'e', 'f'], 'Callback sort failed.');
+    }
+
+    public function testHeapSorted()
+    {
+        $unsorted = DoublyLinkedList::fromArray(['f', 'c', 'a', 'b', 'e', 'd']);
+
+        $heapSorted = $unsorted->heapSorted(new BasicHeap());
+        $this->assertSame($heapSorted->toArray(), ['a', 'b', 'c', 'd', 'e', 'f'], 'Heap sort failed.');
+    }
 }
